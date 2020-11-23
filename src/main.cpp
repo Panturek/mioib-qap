d0c2e8188885ee876d4f6458b33eb0cfdaf7daa9
avamys <dominik.grzegorzewicz@gmail.com>
2020-11-23 17:24:32 +0100

    local search

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <fstream>
 #include <string>
+#include <ctime>
 
 std::vector<int> permutacja(unsigned const int & n)
 {
@@ -62,24 +63,86 @@ int getCost(std::vector<int> &perm, std::vector< std::vector<int> > &P, std::vec
 	{
 		for (int j = 0; j < perm.size(); j++)
 		{
-			cost += P[i][j] * L[perm[i] - 1][perm[j] - 1];
+			cost += P[i][j] * L[perm[i]][perm[j]];
 		}
 	}
 	return cost;
+}
+
+std::vector<int> local_search_greedy(unsigned const int &n, std::vector< std::vector<int> > &P, std::vector< std::vector<int> > &L)
+{
+	std::vector<int> start = permutacja(n);
+	std::vector<int> act = start;
+	bool finish = false;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = i; j < n; j++)
+		{
+			std::vector<int> y(act);
+			std::swap(y[i], y[j]);
+			if (getCost(y, P, L) < getCost(act, P, L))
+			{
+				act = y;
+				finish = true;
+				break;
+			}
+		}
+		if (finish == true)
+		{
+			break;
+		}
+	}
+	return act;
+}
+
+std::vector<int> local_search_steepest(unsigned const int &n, std::vector< std::vector<int> > &P, std::vector< std::vector<int> > &L)
+{
+	std::vector<int> start = permutacja(n);
+	std::vector<int> act = start;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = i; j < n; j++)
+		{
+			std::vector<int> y(act);
+			std::swap(y[i], y[j]);
+			if (getCost(y, P, L) < getCost(act, P, L))
+			{
+				act = y;
+			}
+		}
+	}
+	return act;
 }
 
 int main()
 {
 	srand(time(NULL));
 	//std::vector<int> all = permutacja(26);
-	std::vector<int> all = { 11, 15,  26,   7,   4,  13,  12,   2,   6,  18,   9,   5,   1,  21,   8,
-		14,   3,  20,  19,  10,  17,  25,  16,  24,  22,  23 };
+	//std::vector<int> all = { 11, 15,  26,   7,   4,  13,  12,   2,   6,  18,   9,   5,   1,  21,   8,
+	//	14,   3,  20,  19,  10,  17,  25,  16,  24,  22,  23 };
 
 	std::vector<std::vector<int>> facilities;
 	std::vector<std::vector<int>> locations;
 	readData("data/bur26a.dat", facilities, locations);
-	int cost = getCost(all, facilities, locations);
-	std::cout << cost;
+
+	std::vector<int> solution;
+
+	clock_t begin = clock();
+	int l = 0;
+	do
+	{
+		solution = local_search_steepest(26, facilities, locations);
+		l++;
+	} while (double(clock() - begin) / CLOCKS_PER_SEC < 10);
+
+	double func_time = (double(clock() - begin) / CLOCKS_PER_SEC) / l;
+
+	std::cout << "Performance: " << getCost(solution, facilities, locations) << std::endl;
+	std::cout << "Time: " << func_time << std::endl;
+	std::cout << "l " << l;
+
 	getchar();
 	return 0;
 }
