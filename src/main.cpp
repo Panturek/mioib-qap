26ea4a4a7884d71d04f1e37d0166b157b109c35d
avamys <dominik.grzegorzewicz@gmail.com>
2020-12-23 21:30:39 +0100

    cos

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,33 @@ int main()
 {
 	srand(time(NULL));
 	
-	//std::vector<std::string> files_names = {"chr12a.dat", "chr18a.dat", "bur26a.dat", "esc32e.dat", "lipa50a.dat", "sko100a.dat" };
 
-	std::vector<std::string> files_names = { "chr12a.dat" };
+	std::vector<std::string> files_names = {"chr12a.dat", "chr18a.dat", "bur26a.dat", "esc32e.dat", "nug22.dat",  "tai30b.dat", "tai40b.dat", "tai64c.dat", "wil100.dat" };
 
-	//std::vector<std::string> files_names = {"chr12a.dat", "chr18a.dat", "esc32e.dat", "bur26a.dat", "lipa50a.dat"};
+	//std::vector<std::string> files_names = { "chr12a.dat" };
 
-	std::vector<Algorithm> algos = { randomSearch };
+	std::vector<Algorithm> algos = { tabu };
 
 	// Create directory in results/
-	Procedure proc(algos, files_names, "experiment1");
+	Procedure proc(algos, files_names, "experiment2");
 	proc.run();
 	
+
+	/*
+	QAP qap;
+	qap.readData("data/bur26a.dat");
+	int dim = qap.facilities.size();
+	double sa_init = qap.findTemp();
+
+	Permutation ending_perm;
+	int init_cost;
+	int instance_steps;
+
+	std::tie(ending_perm, init_cost, instance_steps) = qap.tabu(dim);
+
+	std::cout << qap.getCost(ending_perm) << " || " << init_cost << " || " << instance_steps << std::endl;
+	*/
+
 	std::cout << "\n>> Finished all experiments" << std::endl;
 	getchar();
 	return 0;
--- src/procedure.cpp
+++ src/procedure.cpp
@@ -12,7 +12,7 @@ void Procedure::run()
 	for (auto&& algo : algos)
 	{
 
-		if (algo == localGreedy || algo == localSteepest)
+		if (algo == localGreedy || algo == localSteepest || algo == simmulatedAnnealing || algo == tabu)
 		{
 			for (auto&& filename : files_names)
 			{
@@ -24,9 +24,17 @@ void Procedure::run()
 				{
 					std::cout << ">> Finished Greedy on " << filename << std::endl;
 				}
-				else
+				else if (algo == localSteepest)
 				{
 					std::cout << ">> Finished Steepest on " << filename << std::endl;
+				}
+				else if(algo == simmulatedAnnealing)
+				{
+					std::cout << ">> Finished SA on " << filename << std::endl;
+				}
+				else if (algo == tabu)
+				{
+					std::cout << ">> Finished Tabu on " << filename << std::endl;
 				}
 			}
 		}
@@ -35,8 +43,38 @@ void Procedure::run()
 		{
 			for (auto&& filename : files_names)
 			{
+				double algotime = 2;
+
+				if (filename == "chr12a.dat") {
+					algotime = 0.057;
+				}
+				else if (filename == "chr18a.dat") {
+					algotime = 0.27;
+				}
+				else if (filename == "bur26a.dat") {
+					algotime = 1.87;
+				}
+				else if (filename == "esc32e.dat") {
+					algotime = 0.31;
+				}
+				else if (filename == "nug22.dat") {
+					algotime = 1;
+				}
+				else if (filename == "tai30b.dat") {
+					algotime = 5.11;
+				}
+				else if (filename == "tai40b.dat") {
+					algotime = 28.4;
+				}
+				else if (filename == "tai64c.dat") {
+					algotime = 7.54;
+				}
+				else {
+					algotime = 60;
+				}
+
 				QAP qap;
-				random_experiment(filename, qap, algo, "results/"+dirout);
+				random_experiment(filename, qap, algo, "results/"+dirout, algotime);
 
 
 				if (algo == randomSearch)
